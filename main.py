452a63f9a3b888ce4adc22840687924a2b47f91e
drakeepstein35-ctrl <drakeepstein35@gmail.com>
2026-04-28 02:53:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -85,5 +85,4 @@ client = MyClient(intents=intents)
 async def on_ready():
     print(f" Logged in as {client.user} ({client.user.id})")
 
-client.loop.create_task(message_loop())
 client.run(TOKEN, bot=False)
